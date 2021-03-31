2d64c5b8d34e0529cb8baeb15a1f843131318c33
Dave <bgm500844@gmail.com>
2021-03-30 17:57:01 -0700

    Parsing cont

--- Parsing a String to an Integer/Program.cs
+++ Parsing a String to an Integer/Program.cs
@@ -10,6 +10,16 @@ namespace Parsing_a_String_to_an_Integer
     {
         static void Main(string[] args)
         {
+            string myString = "15";
+            string mySecondString = "13";
+            // Parsing Strings into numbers
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+
+            int result = num2 + num1;
+
+            Console.WriteLine(result);
+            Console.Read();
         }
     }
 }
