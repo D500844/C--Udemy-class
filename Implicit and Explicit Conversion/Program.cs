e9cdfa15941d8b3109dd67c2fef444010c62b5a6
Dave <bgm500844@gmail.com>
2021-03-30 17:53:53 -0700

    Conversion and Parsing

---
+++ Implicit and Explicit Conversion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit_and_Explicit_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit Conversion
+            int num = 12345;
+            long bigNum = num;
+
+            float myFloat = 13.37f;
+            double myNewDouble = myFloat;
+
+            // Explicit Conversion
+            double myDouble = 13.37;
+            int myInt;
+
+            // Cast double to int;
+            myInt = (int)myDouble;
+
+            // Type conversion
+            string myString = myDouble.ToString(); // "13.37"
+            num.ToString();
+            myFloat.ToString();
+            bool sunIsShining = true;
+            sunIsShining.ToString();
+
+            Console.WriteLine(sunIsShining);
+            Console.WriteLine(myFloat);
+            Console.WriteLine(myInt);
+            Console.Read();
+            
+        }
+    }
+}
---
+++ Parsing a String to an Integer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_a_String_to_an_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
