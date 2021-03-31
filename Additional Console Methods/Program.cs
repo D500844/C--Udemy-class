05e1a22fc14a2680b47d9528218bb3cdc1c35970
Dave <bgm500844@gmail.com>
2021-03-30 17:14:23 -0700

    Exploring in depth Console Method and standards for variables, methods and class names

---
+++ Additional Console Methods/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Console_Methods
+{
+    // Class names use ClientActivity use PascalCase
+    class BigThing
+    {
+        // Method names like BigBooty also use PascalCase
+        // Arguements/Method Arguements like firstTendies use camelCasing
+        static void Main(string[] bigolItties)
+        {
+            // Local variables like itemBalls use PascalCase
+            // User control like ultimateFaperoth use camelCase
+            // Don't use abbreviations
+            // Don't use numbers at the start of names
+            
+            /*Correct
+             * string myName;
+             * int lastNum;
+             * bool isSaved;
+             * 
+             * Avoid
+             * String MyName;
+             * Int32 LastNum;
+             * Bool 3IsSaved;
+             */ 
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hello Dave.");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("C# is bae");
+
+            Console.Read();
+        }
+    }
+}
