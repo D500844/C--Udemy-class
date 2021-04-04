be238f3eedd39170fd3e9c61be3e46b4f2e90c8e
Dave <bgm500844@gmail.com>
2021-04-03 17:58:45 -0700

    decision making intro

---
+++ Logic and If Intro/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic_and_If_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's the temperature at # wise?: ");
+            string tempUserInput = Console.ReadLine();
+            int temperature = int.Parse(tempUserInput);
+
+
+            if(temperature < 10)
+            {
+                Console.WriteLine("Take the coat");
+            }
+
+            if(temperature == 10)
+            {
+                Console.WriteLine("It's 10 degrees C*");
+            }
+
+            if(temperature > 10)
+            {
+                Console.WriteLine("Cozy & warm!");
+            }
+            Console.Read();
+        }
+    }
+}
